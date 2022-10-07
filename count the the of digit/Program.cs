74356b0d973622df278386d2d778b032f5a4aee1
akashkuchankar <kuchankarakash@gmail.com>
2022-10-07 05:01:48 -0700

    Add project files.

---
+++ count the the of digit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_the_the_of_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  num,count=0;
+            Console.WriteLine("enter multiple digit number");
+            num = Convert.ToInt32(Console.ReadLine());
+           
+                while(num > 0)
+                {
+                    num = num / 10;
+                    count++;
+                }
+                {
+                    Console.WriteLine("number of digit"+count);
+                }
+          
+            Console.ReadLine();
+        }
+    }
+}

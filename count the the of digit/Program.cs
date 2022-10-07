7996fd7f3dbde5d628776f165911adc7205f622a
akashkuchankar <kuchankarakash@gmail.com>
2022-10-07 05:37:02 -0700

    power

--- count the the of digit/Program.cs
+++ count the the of digit/Program.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace count_the_the_of_digit
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int  num,count=0;
-            Console.WriteLine("enter multiple digit number");
-            num = Convert.ToInt32(Console.ReadLine());
+//namespace count_the_the_of_digit
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int  num,count=0;
+//            Console.WriteLine("enter multiple digit number");
+//            num = Convert.ToInt32(Console.ReadLine());
            
-                while(num > 0)
-                {
-                    num = num / 10;
-                    count++;
-                }
-                {
-                    Console.WriteLine("number of digit"+count);
-                }
+//                while(num > 0)
+//                {
+//                    num = num / 10;
+//                    count++;
+//                }
+//                {
+//                    Console.WriteLine("number of digit"+count);
+//                }
           
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}

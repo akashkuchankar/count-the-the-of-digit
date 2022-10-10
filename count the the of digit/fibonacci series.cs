6b7a04ee82ee181092a734b3e11495a23c06ae97
akashkuchankar <kuchankarakash@gmail.com>
2022-10-10 00:15:48 -0700

    power

---
+++ count the the of digit/fibonacci series.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_the_the_of_digit
+{
+    internal class fibonacci_series
+    {
+    }
+}
---
+++ count the the of digit/sum of digit.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace count_the_the_of_digit
+//{
+//    internal class sum_of_digit
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num, remainder, sum = 0;
+//            Console.WriteLine("Enter multiple digit number ");
+//            num = Convert.ToInt32(Console.ReadLine());
+//            while (num > 0)
+//            {
+//                remainder = num % 10;
+//                sum = sum + remainder;
+//                num = num / 10;
+
+//            }
+//            Console.WriteLine($"sum of digit is {sum}");
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
